1b30489ca746163a8684cd54edd16dd6959803d4
Tarek Ahmed <tarek5701@gmail.com>
2020-11-29 10:21:42 -0800

    Added music in notification activity and fixed error in date picker

--- app/src/main/java/com/csis_3175_070/birthdayreminderapp/DOBAddActivity.java
+++ app/src/main/java/com/csis_3175_070/birthdayreminderapp/DOBAddActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +20,6 @@ public class DOBAddActivity extends AppCompatActivity {
     EditText fName, lName, date;
     Button addButton;
     DBHelper DB;
-    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +39,28 @@ public class DOBAddActivity extends AppCompatActivity {
                 String FirstN = fName.getText().toString();
                 String LastN = lName.getText().toString();
                 String Date = date.getText().toString();
-                DBHelper myDB = new DBHelper(DOBAddActivity.this);
-                myDB.addDob(FirstN, LastN, Date);
-                finish();
+                if(FirstN.isEmpty() || LastN.isEmpty() || Date.isEmpty()){
+                    Toast.makeText(getBaseContext(),"Please fill up all the information properly",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    DBHelper myDB = new DBHelper(DOBAddActivity.this);
+                    myDB.addDob(FirstN, LastN, Date);
+                    finish();
+                }
             }
         });
 
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Calendar calendar = Calendar.getInstance();
-                int mYear = calendar.get(Calendar.YEAR);
-                int mMonth = calendar.get(Calendar.MONTH);
-                int mDay = calendar.get(Calendar.DAY_OF_MONTH);
-                datePickerDialog = new DatePickerDialog(DOBAddActivity.this,
-                        (view, year, monthOfYear, dayOfMonth) -> {
-                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yy");
-                            date.setText(simpleDateFormat.format(calendar.getTime()));
-
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-            }
+        date.setOnClickListener(v -> {
+            final Calendar calendar = Calendar.getInstance();
+            DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yy");
+                date.setText(simpleDateFormat.format(calendar.getTime()));
+            };
+            new DatePickerDialog(DOBAddActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
         });
     }
 }
+
--- app/src/main/java/com/csis_3175_070/birthdayreminderapp/ReminderNotification.java
+++ app/src/main/java/com/csis_3175_070/birthdayreminderapp/ReminderNotification.java
@@ -3,10 +3,12 @@ package com.csis_3175_070.birthdayreminderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class ReminderNotification extends AppCompatActivity {
+//    MP3 Downloaded from = https://www.zedge.net/ringtone/c0c8b6a7-78e2-33ee-8970-3d98c38a162b
     TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,8 @@ public class ReminderNotification extends AppCompatActivity {
         String message = intent.getStringExtra("Message");
         title = findViewById(R.id.textViewTitleReminder);
         title.setText(message);
+
+        MediaPlayer mPlayer = MediaPlayer.create(ReminderNotification.this, R.raw.happy_birthday_tone);
+        mPlayer.start();
     }
 }
